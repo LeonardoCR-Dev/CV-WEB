4a19fe251f9d9968c6e8254f3b638808f32ee46e
Leonardo Cruz <cruzleonardo00@gmail.com>
2020-07-24 18:31:29 -0500

    Primer Commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//ScrollReveal().reveal('.principal');
+/*
+ScrollReveal().reveal('.caja', {
+	interval: 16,
+	reset: true
+});
+ScrollReveal().reveal('.tarjeta', {
+	interval: 16,
+	reset: true
+});*/
